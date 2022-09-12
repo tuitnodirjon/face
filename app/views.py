ae4a5df58d64d27b48127c5909bb0132252672c4
nodirjon <ruzimurodovnodir66@gmail.com>
2022-09-12 14:19:44 +0500

    api updated

--- app/views.py
+++ app/views.py
@@ -14,34 +14,27 @@ def check_image(request):
     if request.method == 'POST':
         # print(json.loads(request.body))
         data = json.loads(request.body)
-        img_data = data['image']
-        # pinfl = data['pinfl']
-        # profile =
+        face_img_data = data['face_image']
+        base_img_data = data['base_image']
         import base64
         import random
-        image_name = f"image_{random.randint(1, 100000)}"
-        decodeit = open(f'{image_name}.jpg', 'wb')
-        decodeit.write(base64.b64decode(img_data.split(',')[-1]))
-        decodeit.close()
-        image2_test = face_recognition.load_image_file(f'{image_name}.jpg')
-        image2_face_encoding = face_recognition.face_encodings(image2_test)[0]
-        images = os.listdir('imagesBasic')
-        known_face_encodings = []
-        known_face_names = []
-        for i in images:
-            image = face_recognition.load_image_file("imagesBasic/" + i)
-            face_encoding = face_recognition.face_encodings(image)[0]
-            known_face_encodings.append(face_encoding)
-            known_face_names.append(i.split('.')[0])
-        results = compare_faces(known_face_encodings, image2_face_encoding)
+        face_image_name = f"images/image_{random.randint(1, 10000)}"
+        base_image_name = f"images/image_{random.randint(10001, 100000)}"
+        face_decodeit = open(f'{face_image_name}.jpg', 'wb')
+        face_decodeit.write(base64.b64decode(face_img_data.split(',')[-1]))
+        face_decodeit.close()
+        base_decodeit = open(f'{base_image_name}.jpg', 'wb')
+        base_decodeit.write(base64.b64decode(base_img_data.split(',')[-1]))
+        base_decodeit.close()
+        face_image = face_recognition.load_image_file(f'{face_image_name}.jpg')
+        base_image = face_recognition.load_image_file(f'{base_image_name}.jpg')
+        face_encoding = face_recognition.face_encodings(face_image)[0]
+        base_encoding = face_recognition.face_encodings(base_image)[0]
+        results = compare_faces([base_encoding], face_encoding)
         print(results)
         if True in results:
-            first_match_index = results.index(True)
-            name = known_face_names[first_match_index]
-            print(name)
-        else:
-            name = "Unknown"
-        return Response({'name': name})
+            return Response({'result': True})
+        return Response({'result': False})
 
 
 def check_face(request):
